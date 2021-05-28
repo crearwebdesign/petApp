5ae37dc7d985324979c75c517b220177abb7f2fe
crearwebdesign <54069542+crearwebdesign@users.noreply.github.com>
2021-05-28 08:12:57 -0500

    Integracion con firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAU-BvHdViLHG8kN0EICbiaA1gfZhavtiM",
+    authDomain: "db-petapp.firebaseapp.com",
+    projectId: "db-petapp",
+    storageBucket: "db-petapp.appspot.com",
+    messagingSenderId: "141443970977",
+    appId: "1:141443970977:web:3143711ad7951e9129bde0",
+    measurementId: "G-8L9RFDSHNW"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAU-BvHdViLHG8kN0EICbiaA1gfZhavtiM",
+    authDomain: "db-petapp.firebaseapp.com",
+    projectId: "db-petapp",
+    storageBucket: "db-petapp.appspot.com",
+    messagingSenderId: "141443970977",
+    appId: "1:141443970977:web:3143711ad7951e9129bde0",
+    measurementId: "G-8L9RFDSHNW"
+  }
 };
 
 /*
