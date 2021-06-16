f1bf40d1cc5ac2d931b9bfee53e2ffbe0f730d96
crearwebdesign <54069542+crearwebdesign@users.noreply.github.com>
2021-06-16 08:39:10 -0500

    Datos para perfil usuario

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { PerfilComponent } from './perfil/perfil.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent,
+  PerfilComponent],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    RouterModule,
+    FormsModule
   ]
 })
 export class PagesModule { }
---
+++ src/app/pages/perfil/perfil.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.component.html',
+  styleUrls: ['./perfil.component.scss'],
+})
+export class PerfilComponent implements OnInit {
+
+  constructor(public menucontroller : MenuController) { }
+
+  ngOnInit() {}
+
+  openMenu() {
+    this.menucontroller.toggle('principal');
+  }
+
+}
