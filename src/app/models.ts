a4cc041daee8aad017faf963a86c8cc9fbdad8e3
crearwebdesign <54069542+crearwebdesign@users.noreply.github.com>
2021-06-17 08:54:53 -0500

    Interface Modelo Cliente

--- src/app/models.ts
+++ src/app/models.ts
@@ -8,3 +8,14 @@ export interface Producto {
     id : string;
     fecha : Date;
 }
+
+export interface Cliente {
+    uid : string;
+    email : string;
+    nombre : string;
+    celular : string;
+    foto : string;
+    referencia : string;
+    ubicacion : any;
+
+}
--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { Cliente } from 'src/app/models';
 
 @Component({
   selector: 'app-perfil',
@@ -8,12 +9,40 @@ import { MenuController } from '@ionic/angular';
 })
 export class PerfilComponent implements OnInit {
 
+  cliente : Cliente = {
+    uid : '',
+    email : '',
+    nombre : '',
+    celular : '',
+    foto : '',
+    referencia : '',
+    ubicacion : '',
+  }
+
+  newFile : any;
+
   constructor(public menucontroller : MenuController) { }
 
   ngOnInit() {}
 
   openMenu() {
     this.menucontroller.toggle('principal');
+  }
+
+  async newImageUpload( event : any){
+    if (event.target.files && event.target.files[0]){
+      this.newFile = event.target.files[0];
+      const reader = new FileReader();
+      reader.onload = ( (image) => {
+        this.cliente.foto = image.target.result as string;
+      } );
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
+
+
+  registrarse(){
+    
   }
 
 }
