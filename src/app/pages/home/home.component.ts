3c90a1b3f7b546a4987f4eec1db6b5599f2d3aea
crearwebdesign <54069542+crearwebdesign@users.noreply.github.com>
2021-06-27 09:15:29 -0500

    Mostrar productos en homePage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { Producto } from 'src/app/models';
 import { FirebaseauthService } from 'src/app/services/firebaseauth.service';
+import { FirestoreService } from '../../services/firestore.service';
+
 
 @Component({
   selector: 'app-home',
@@ -10,20 +13,32 @@ import { FirebaseauthService } from 'src/app/services/firebaseauth.service';
 export class HomeComponent implements OnInit {
 
   uid = '';
-  constructor( public menucontroller : MenuController,
-    public firebaseauthService : FirebaseauthService ) { 
-    this.firebaseauthService.stateAuth().subscribe(res=>{
-      if(res !== null){
+  private path = 'productos/';
+  articulos : Producto[] = [];
+  constructor(public menucontroller: MenuController,
+    public firestoreService : FirestoreService,
+    public firebaseauthService: FirebaseauthService) {
+
+    this.firebaseauthService.stateAuth().subscribe(res => {
+      if (res !== null) {
         this.uid = res.uid;
-        
+        this.loadProductos();
       }
     });
+    
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   openMenu() {
     this.menucontroller.toggle('principal');
+  }
+
+  loadProductos(){
+      this.firestoreService.getCollection<Producto>(this.path).subscribe( res =>{
+        console.log(res);
+        this.articulos = res;
+      } );
   }
 
 }
