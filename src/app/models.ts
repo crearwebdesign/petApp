1e7d059f63ca9121b580bb966bd6b9bd61c06709
crearwebdesign <54069542+crearwebdesign@users.noreply.github.com>
2021-08-28 08:16:33 -0500

    Ver pedidos nuevos

--- src/app/models.ts
+++ src/app/models.ts
@@ -26,7 +26,7 @@ export interface Pedido {
     productos : ProductoPedido[];
     precioTotal : number;
     estado : EstadoPedido;
-    fecha : Date;
+    fecha : any;
     valoracion : number;
 }
 
